b6e32a0af706e6b680a9aae7b58b81ebd42f02bb
Davide B <inbox@davideb.net>
2021-01-12 13:12:48 +0100

    Merda Pura X

---
+++ davidb003/Java/HardDisk/src/com/school_HDD/Main.java
@@ -0,0 +1,31 @@
+package com.school_HDD;
+import
+public class Main {
+
+    public static void main(String[] args) {
+	    int minuti = 5;
+	    int secondi = 0;
+	    int punteggio = 0;
+
+
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (minuti - 1; ) {
+
+        }
+
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (punteggio++) {
+
+        }
+    }
+}
