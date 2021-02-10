1b34fbd429aabb30c635ec4876fb77597fa0da82
Davide B <inbox@davideb.net>
2021-02-10 11:39:11 +0100

    Programma decente
    Manca:
    - Generatore numeri randomici

--- davidb003/Java/arrays/src/com/school_Coding_Arrays/Main.java
+++ davidb003/Java/arrays/src/com/school_Coding_Arrays/Main.java
@@ -1,5 +1,4 @@
 package com.school_Coding_Arrays;
-import java.util.Random;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -8,47 +7,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner reqInput = new Scanner(System.in);
 
-        Random rand = new Random();
-        int upperbound = 25;
-        int int_random = rand.nextInt(upperbound);
-        System.out.println(upperbound);
+        Object array[] = {10,2,22,69};
 
-        System.out.print("Enter no. of elements you want in array:");
-
-        int NumElements;
-        int ArrayLenght = 0;
-        int sum = 0;
-        NumElements = reqInput.nextInt();
-
-        int array[] = new int[NumElements];
-
-
-        for(int i = 0; i < NumElements; i++)
-        {
-            array[i] = int_random;
-        }
-
-
-
-
-        // sorting array
         Arrays.sort(array);
 
-        // let us print all the elements available
-        System.out.println("Array ridordinato con successo nella seguente posizione: ");
+        System.out.println("Riordiamento array:");
         for (Object number : array) {
             System.out.println(number);
         }
 
-        // entering the value to be searched
-        int searchVal = 22;
+        int searchVal;
+        System.out.println("Cerca nell'array: ");
+        searchVal = reqInput.nextInt();
+
 
         int retVal = Arrays.binarySearch(array,searchVal);
 
-        int finalVal;
-        finalVal = retVal + 1;
-
-        System.out.println(searchVal + " si trova nella posizione numero: " + finalVal);
-
+        int finalRetVal;
+        finalRetVal = retVal + 1;
+        System.out.println("The index of element 22 is : " + finalRetVal);
     }
 }
