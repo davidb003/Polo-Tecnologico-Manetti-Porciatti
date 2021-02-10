3a72bead462361051db2efa05525e2819a27601c
Davide B <inbox@davideb.net>
2021-02-10 10:03:34 +0100

    questo codice fa sboccare

--- davidb003/Java/arrays/src/com/school_Coding_Arrays/Main.java
+++ davidb003/Java/arrays/src/com/school_Coding_Arrays/Main.java
@@ -1,23 +1,54 @@
 package com.school_Coding_Arrays;
 import java.util.Random;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        test Test = new test();
-
-        int[] array;
+        Scanner reqInput = new Scanner(System.in);
 
         Random rand = new Random();
-        int outputRand = rand.nextInt(5);
-        for (int i = 0; i < args.length; i++) {
-            array[i] = outputRand;
+        int upperbound = 25;
+        int int_random = rand.nextInt(upperbound);
+        System.out.println(upperbound);
+
+        System.out.print("Enter no. of elements you want in array:");
+
+        int NumElements;
+        int ArrayLenght = 0;
+        int sum = 0;
+        NumElements = reqInput.nextInt();
+
+        int array[] = new int[NumElements];
+
+
+        for(int i = 0; i < NumElements; i++)
+        {
+            array[i] = int_random;
         }
 
+
+
+
+        // sorting array
         Arrays.sort(array);
 
-        System.out.println(array + " found at index = " + Arrays.binarySearch(array,5));
+        // let us print all the elements available
+        System.out.println("Array ridordinato con successo nella seguente posizione: ");
+        for (Object number : array) {
+            System.out.println(number);
+        }
+
+        // entering the value to be searched
+        int searchVal = 22;
+
+        int retVal = Arrays.binarySearch(array,searchVal);
+
+        int finalVal;
+        finalVal = retVal + 1;
+
+        System.out.println(searchVal + " si trova nella posizione numero: " + finalVal);
 
     }
 }
