30a853b4f78f2f5b541834fcd0e4172d21fac59c
Davide B <inbox@davideb.net>
2021-02-18 14:25:46 +0100

    Why do you revive the trash can?

--- davidb003/Java/ClassArrayObject/src/com/school_Coding_ClassArrayObject/Main.java
+++ davidb003/Java/ClassArrayObject/src/com/school_Coding_ClassArrayObject/Main.java
@@ -7,5 +7,16 @@ public class Main {
         Students students = new Students();
         Class studentsArrayObj[]= new Class[i];
 
+        // TODO: Mettere l'input perchè se no
+
+        for(i = 0; i < arrayLenght ; i++)
+        {
+            array[i] = rand.nextInt(arrayLenght);
+            System.out.println(array[i]);
+            sum = sum + array[i];
+
+        }
+
+
     }
 }
