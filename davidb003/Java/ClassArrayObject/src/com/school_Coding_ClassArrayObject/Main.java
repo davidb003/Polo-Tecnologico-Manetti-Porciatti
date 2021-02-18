865ce602f115aec15db10e3e9687f8418116deac
Davide B <inbox@davideb.net>
2021-02-18 14:21:19 +0100

    Trash

---
+++ davidb003/Java/ClassArrayObject/src/com/school_Coding_ClassArrayObject/Main.java
@@ -0,0 +1,11 @@
+package com.school_Coding_ClassArrayObject;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int i = 0;
+        Students students = new Students();
+        Class studentsArrayObj[]= new Class[i];
+
+    }
+}
---
+++ davidb003/Java/ClassArrayObject/src/com/school_Coding_ClassArrayObject/Students.java
@@ -0,0 +1,10 @@
+package com.school_Coding_ClassArrayObject;
+
+public class Students {
+    public String name;
+    public String numberOfStudents;
+
+    public void addStudent(String addStudent_Name) {
+
+    }
+}
