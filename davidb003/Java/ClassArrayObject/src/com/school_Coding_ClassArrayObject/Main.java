825ec767ea6934ad3b831c9ff2141ccbfb334987
Davide B <inbox@davideb.net>
2021-02-23 10:35:24 +0100

    Quello di matematica è caghino

--- davidb003/Java/ClassArrayObject/src/com/school_Coding_ClassArrayObject/Main.java
+++ davidb003/Java/ClassArrayObject/src/com/school_Coding_ClassArrayObject/Main.java
@@ -1,34 +1,35 @@
 package com.school_Coding_ClassArrayObject;
 
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-	    byte arrayLenght = 0;
-	    int i = 0;
+        int arrayLenght;
+        int i;
 
         Scanner reqInput = new Scanner(System.in);
-        Students students = new Students();
-        Random rand = new Random();
 
-        System.out.println("Quanti studenti ci sono in classe? > ");
-        arrayLenght = reqInput.nextByte();
+        System.out.println("Quanti studenti ci sono? >");
+        arrayLenght = reqInput.nextInt();
 
-        Class studentsArrayObj[]= new Class[arrayLenght];
+        String[] studenti = new String[arrayLenght];
 
-
-        // TODO Questo programma è lo sbocco
-        int studentAdd;
-        for(i = 0; i < arrayLenght ; i++)
-        {
-            studentAdd =
-            System.out.println(array[i]);
-            sum = sum + array[i];
-
+        for (i = 0; i < arrayLenght; i++) {
+            studenti[i] = reqInput.next();
         }
 
+        // Aggiungi Studenti
+        System.out.println("Cerca uno studente >");
+        String searchedInput;
+        searchedInput = reqInput.next();
+        boolean found = Arrays.stream(studenti).anyMatch(searchedInput::equals);
+        if(found){
+            for (i = 0; i < arrayLenght; i++) {
+                studenti[i] = reqInput.next();
+            }
+        }
 
     }
 }
